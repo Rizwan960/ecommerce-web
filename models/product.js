46736d86ea85424ba7465626f39c4c253135d956
Rizwan Ali <softtechnode@gmail.com>
2024-08-23 17:44:50 +0500

    Project initilization

---
+++ models/product.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const db =require('../util/databas')
+const Cart = require('./cart');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+ 
+
+module.exports = class Product {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+
+  }
+
+  static deleteById(id) {
+   
+  }
+
+  static fetchAll() {
+   return db.execute('SELECT * FROM products');
+  }
+
+  static findById(id, cb) {
+  }
+};
