55416e6bc6a562a00a567fbc496e12f458090588
Rizwan Ali <softtechnode@gmail.com>
2024-09-03 21:56:31 +0500

    File upload and download also genreate pdf added

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,13 +6,30 @@ const fs = require('fs')
 const path = require('path');
 const PDFDocument = require('pdfkit')
 
+const ITEMS_PER_PAGE=2;
+
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  const page= +req.query.page||1;
+  let totalItems;
+
+  Product.find().countDocuments().then(numberOfProducts=>{
+    totalItems=numberOfProducts
+    return  Product.find()
+    .skip((page-1)*ITEMS_PER_PAGE )
+    .limit(ITEMS_PER_PAGE);
+  })
   .then(products=>{
     res.render('shop/product-list', {
       prods: products,
       pageTitle: 'All Products',
       path: '/products',
+      totalProducts:totalItems,
+      currentPage: page,
+      hasNextPage:ITEMS_PER_PAGE*page<totalItems,
+      hasPreviousPage: page>1,
+      nextPage:page+1,
+      previousPage:page-1,
+      lastPage: Math.ceil(totalItems/ITEMS_PER_PAGE)
      
     });
   })
@@ -20,12 +37,27 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.find()
+  const page= +req.query.page||1;
+  let totalItems;
+  Product.find().countDocuments()
+  .then(numberOfProducts=>{
+    totalItems=numberOfProducts
+    return  Product.find()
+    .skip((page-1)*ITEMS_PER_PAGE )
+    .limit(ITEMS_PER_PAGE);
+  })
   .then(products=>{
     res.render('shop/index', {
       prods: products,
       pageTitle: 'Shop',
       path: '/',
+      totalProducts:totalItems,
+      currentPage: page,
+      hasNextPage:ITEMS_PER_PAGE*page<totalItems,
+      hasPreviousPage: page>1,
+      nextPage:page+1,
+      previousPage:page-1,
+      lastPage: Math.ceil(totalItems/ITEMS_PER_PAGE)
     
     });
   })
